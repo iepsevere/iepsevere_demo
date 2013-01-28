90c64dd57d0312c6c231a35644633247f236beaf
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2013-01-28 20:42:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IepsevereDemo::Application.config.secret_token = 'f1dcb7608af69755aa2fe2bc6559bc1863c9b697f1ee9c1b6346e973ac3450a2d6b78503f4c30fd47f5e3589ab8c57553c71039c807681d6adade6eb33185c5d'
