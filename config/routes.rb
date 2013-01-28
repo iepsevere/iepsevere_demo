b8833053bb98da43e0cca183b65504a96c0b18a7
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2013-01-28 20:51:03 +0100

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 IepsevereDemo::Application.routes.draw do
   resources :people
 
-
+  root :to => "people#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
