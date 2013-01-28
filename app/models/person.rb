9c517b51886eadcf3c61200ef449fe6167f478d7
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2013-01-28 20:49:04 +0100

    added person scaffold

---
+++ app/models/person.rb
@@ -0,0 +1,3 @@
+class Person < ActiveRecord::Base
+  attr_accessible :birthdate, :firstname, :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 IepsevereDemo::Application.routes.draw do
+  resources :people
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
