a6451eaf6bce2cdc886dfff31b7aefd45714c326
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2013-01-28 21:05:28 +0100

    fixing birthdate bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ IepsevereDemo::Application.routes.draw do
   resources :people
 
   root :to => "people#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
